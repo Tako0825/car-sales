b6cf71f6a1efe341f179085e19d98db9b47c4809
Tako <2060364922@qq.com>
2023-10-28 13:56:07 +0800

    修改PrismaModel枚举ts

--- src/common/dto/page.dto.ts
+++
@@ -1,9 +0,0 @@
-import { Validate } from "class-validator"
-import { IntegerRule } from "src/common/rule/integer.rule"
-
-export class PageDto {
-    @Validate(IntegerRule, { message: "无法转换为有效整数" })
-    currentPage: number
-    @Validate(IntegerRule, { message: "无法转换为有效整数" })
-    pageSize: number
-}
--- src/common/enum/PrismaModel.ts
+++ src/common/enum/PrismaModel.ts
@@ -1,9 +1,9 @@
 export enum PrismaModel {
     user = "user",
-    course = "course",
-    chapter = "chapter",
-    vedio = "vedio",
-    subscribe = "subscribe",
-    binding = "binding",
-    authApply = "authApply"
+    product = "product",
+    supplier = "supplier",
+    suppliyRelation = "supplyRelation",
+    inventory = "inventory",
+    move = "move",
+    order = "order"
 }
