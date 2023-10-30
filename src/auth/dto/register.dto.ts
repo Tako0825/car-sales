9a20cd99df9aa905d63f432b883e30f989b458ce
Tako <2060364922@qq.com>
2023-10-30 10:52:45 +0800

    补充RegisterDto非空字段管道验证规则

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -13,7 +13,9 @@ export class RegisterDto implements Partial<User> {
     @IsNotEmpty({ message: "确认密码不允许为空" })
     passwordConfirmed: string
     @Validate(PhoneRule, { message: "员工电话格式不符 (以“1”开头共11位数字组合)" })
+    @IsNotEmpty({ message: "员工电话不允许为空" })
     phone: string
     @Validate(RoleRule, { message: "不存在该职位" })
+    @IsNotEmpty({ message: "员工职位不允许为空" })
     role: $Enums.Role
 }
