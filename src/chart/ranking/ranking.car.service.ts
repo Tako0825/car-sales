16a19b4e20e7c97af96813a290f563b27b10e0df
Tako <2060364922@qq.com>
2023-11-06 09:53:09 +0800

    修复折线图数据错位问题（需要考虑销量为0的情况）

--- src/chart/ranking/ranking.car.service.ts
+++ src/chart/ranking/ranking.car.service.ts
@@ -43,36 +43,46 @@ export class RankingCarService {
 
             // 近 year 年以来销量最高的 count 台汽车的 “每年销量” 列表
             const result:Array<ISource> = await this.prisma.$queryRaw`
-                SELECT sub1.productId,
-                sub1.product,
-                sub1.currentyear,
-                CONCAT(sub1.yearsales, '') AS sales
-                FROM (
-                    SELECT o.productId,
-                    CONCAT(product.name,"-",product.model) AS product,
-                    YEAR(o.createtime) AS currentyear,
-                    count(*) AS yearsales
-                    FROM \`order\` AS o 
-                    INNER JOIN product 
-                    ON o.productId = product.id
-                    GROUP BY o.productId, currentyear
-                ) AS sub1
-                INNER JOIN (
-                    SELECT o.productId, count(*) AS sales
-                    FROM \`order\` AS o 
-                    WHERE year(o.createtime) > ${currentYear - year}
-                    GROUP BY o.productId
-                    ORDER BY sales DESC, productId ASC
-                    LIMIT ${count}
-                ) AS sub2
-                ON sub1.productId = sub2.productId
-                HAVING sub1.currentyear > ${currentYear - year}
-                ORDER BY currentyear, sales DESC, productId ASC
+					SELECT sub1.productId,
+					sub1.product,
+					sub1.currentyear,
+					CONCAT(sub1.yearsales, '') AS sales
+					FROM (
+						SELECT p.id AS productId,
+						CONCAT(p.name,"-",p.model) AS product,
+						y.year AS currentyear,
+						IFNULL(o.yearsales, 0) AS yearsales
+						FROM product AS p
+						CROSS JOIN (
+						SELECT DISTINCT YEAR(createtime) AS year
+						FROM \`order\`
+						) AS y
+						LEFT JOIN (
+						SELECT productId, YEAR(createtime) AS year, COUNT(*) AS yearsales
+						FROM \`order\`
+						GROUP BY productId, year
+						) AS o
+						ON p.id = o.productId AND y.year = o.year
+						ORDER BY p.id, y.year
+					) AS sub1
+					INNER JOIN (
+							SELECT o.productId, count(*) AS sales
+							FROM \`order\` AS o 
+							WHERE year(o.createtime) > 2013
+							GROUP BY o.productId
+							ORDER BY sales DESC, productId ASC
+							LIMIT 7
+					) AS sub2
+					ON sub1.productId = sub2.productId
+					HAVING sub1.currentyear > 2013
+					ORDER BY currentyear, sub2.sales DESC, productId ASC
             `
             const source:Array<[any, any, any]> = [["sales", "product", "year"]]
             result.map(item => {
                 source.push([ +item.sales, item.product, item.currentyear ])
             })
+            console.log(source);
+            
 
             return {
                 tip: "成功获取汽车热销榜",
