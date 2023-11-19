81a458df59d3bdb1c155b8d634a49b8010d7601a
Tako <2060364922@qq.com>
2023-11-19 14:09:23 +0800

    优化登录界面-输入框样式&加载动画

--- vue/src/layouts/login/LoginForm.vue
+++ vue/src/layouts/login/LoginForm.vue
@@ -1,20 +1,54 @@
 <template>
-    <main class="flex flex-col gap-6 transform scale-105">
+    <main class="flex flex-col gap-6 transform">
         <section class="w-full h-auto relative">
-            <input type="text" placeholder="enter your phone number" class="w-full h-auto p-3 border-2 border-solid border-gray-400 rounded-md font-bold text-lg">
+            <input type="text" v-model="phone" placeholder="enter your phone number" class="w-full h-auto p-3 border-2 border-solid border-gray-400 rounded-md font-bold text-md">
             <span class="text-xs font-bold absolute top-0 left-4 transform -translate-y-1/2 bg-white text-gray-400 px-2">用户名</span>
         </section>
         <section class="w-full h-auto relative">
-            <input type="password" placeholder="enter your password" class="w-full h-auto p-3 border-2 border-solid border-gray-400 rounded-md font-bold text-lg">
+            <input type="password" v-model="password" placeholder="enter your password" class="w-full h-auto p-3 border-2 border-solid border-gray-400 rounded-md font-bold text-md">
             <span class="text-xs font-bold absolute top-0 left-4 transform -translate-y-1/2 bg-white text-gray-400 px-2">密&nbsp;&nbsp;&nbsp;码</span>
         </section>
-        <el-button class="login-button" type="primary" icon="el-icon-right">登 录</el-button>
+        <el-button v-loading.fullscreen="!dataReady" :disabled="!dataReady" @click="login" class="login-button" type="primary" icon="el-icon-right">登 录</el-button>
     </main>
 </template>
 
 <script>
+import { sleep } from '@/util/sleep'
+
 export default {
-    name: "LoginForm"
+    name: "LoginForm",
+    data() {
+        return {
+            phone: "",
+            password: "",
+            dataReady: true
+        }
+    },
+    methods: {
+        async login() {
+            // 手机号 & 密码
+            const phone = this.phone
+            const password = this.password
+            this.dataReady = false
+            // 加载动画
+            await sleep(500)
+            this.dataReady = true
+            // 表单验证验证规则
+            if(!phone || !password) {
+                return this.$notify.error({
+                    title: '手机号/密码不允许为空',
+                })
+            }
+            // todo-登录请求(1.token本地存储、2.跳转路由至工作台)
+            
+            // 清空表单
+            this.removeForm()
+        },
+        async removeForm() {
+            this.phone = ''
+            this.password = ''
+        } 
+    }
 }
 </script>
 
