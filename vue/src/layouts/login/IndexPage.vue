562eef25ca87b568d5774f5231dac464a5083c1b
Tako <2060364922@qq.com>
2023-11-19 13:39:32 +0800

    登录界面UI-finished

--- vue/src/layouts/login/IndexPage.vue
+++ vue/src/layouts/login/IndexPage.vue
@@ -1,9 +1,24 @@
 <template>
-  <main class="w-full h-full overflow-hidden flex justify-center items-center bg-white">
-    <article class="w-1/3 h-auto flex flex-col transform -translate-y-20 gap-4">
-      <h1>123</h1>
-      <h2>456</h2>
-      <LoginForm class="w-full h-auto p-4 rounded-lg" :style="{ 'boxShadow': '0 10px 110px 1px rgba(0,0,0,.1)' }"/>
+  <main class="w-full h-full overflow-hidden flex bg-white">
+    <!-- 左半部分 -->
+    <aside class="w-5/12 h-full flex flex-col justify-center items-center" style="background-color: #a162f7;">
+      <el-image :src="'http://cdn.takoko.top/CarSale%20Poster.png'" fit="cover" class="transform -translate-y-20"></el-image>
+    </aside>
+    <!-- 右半部分 -->
+    <article class="w-7/12 h-auto flex flex-col gap-16 justify-center items-center transform -translate-y-12">
+      <!-- 头部 -->
+      <header class="flex flex-col gap-2 items-center">
+        <section class="flex gap-2 items-center">
+          <!-- LOGO -->
+          <img src="@/assets/icon/logo.png" alt="LOGO" class="w-16 h-16">
+          <!-- 系统名称 -->
+          <h1 class="font-bold text-3xl">{{ title }}</h1>
+        </section>
+        <!-- 系统描述 -->
+        <span class="fonr-bold text-2xl">{{ introduce }}</span>
+      </header>
+      <!-- 登录表单 -->
+      <LoginForm class="w-96 h-auto rounded-lg"/>
     </article>
   </main>
 </template>
@@ -12,7 +27,13 @@
 import LoginForm from "./LoginForm"
 export default {
   name: "LoginPage",
-  components: { LoginForm }
+  components: { LoginForm },
+  data() {
+    return {
+      title: "Car Sale",
+      introduce: "汽车销售管理平台"
+    }
+  }
 }
 </script>
 
--- vue/src/layouts/login/LoginForm.vue
+++ vue/src/layouts/login/LoginForm.vue
@@ -1,57 +1,32 @@
 <template>
-    <el-form 
-        :model="form" 
-        status-icon 
-        :rules="rules" 
-        ref="form" 
-        label-position="top"
-    >
-        <el-form-item label="手机号" prop="phone">
-            <el-input type="text" v-model="form.phone" autocomplete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="密码" prop="password">
-            <el-input type="password" v-model="form.password" autocomplete="off"></el-input>
-        </el-form-item>
-        <el-form-item>
-            <el-button type="primary" @click="submitForm('form')">提交</el-button>
-            <el-button @click="resetForm('form')">重置</el-button>
-        </el-form-item>
-    </el-form>
+    <main class="flex flex-col gap-6 transform scale-105">
+        <section class="w-full h-auto relative">
+            <input type="text" placeholder="enter your phone number" class="w-full h-auto p-3 border-2 border-solid border-gray-400 rounded-md font-bold text-lg">
+            <span class="text-xs font-bold absolute top-0 left-4 transform -translate-y-1/2 bg-white text-gray-400 px-2">用户名</span>
+        </section>
+        <section class="w-full h-auto relative">
+            <input type="password" placeholder="enter your password" class="w-full h-auto p-3 border-2 border-solid border-gray-400 rounded-md font-bold text-lg">
+            <span class="text-xs font-bold absolute top-0 left-4 transform -translate-y-1/2 bg-white text-gray-400 px-2">密&nbsp;&nbsp;&nbsp;码</span>
+        </section>
+        <el-button class="login-button" type="primary" icon="el-icon-right">登 录</el-button>
+    </main>
 </template>
 
 <script>
 export default {
-    name: "LoginForm",
-    data() {
-        return {
-            form: {
-                phone: '',
-                password: ''
-            },
-            rules: {
-                phone: { required: true, message: '请输入手机号', trigger: 'blur' },
-                password: { required: true, message: '请输入密码', trigger: 'blur' },
-            }
-        };
-    },
-    methods: {
-        submitForm(formName) {
-            this.$refs[formName].validate((valid) => {
-                if (valid) {
-                    alert('submit!');
-                } else {
-                    console.log('error submit!!');
-                    return false;
-                }
-            });
-        },
-        resetForm(formName) {
-            this.$refs[formName].resetFields();
-        }
-    }
+    name: "LoginForm"
 }
 </script>
 
-<style>
+<style scoped>
+    /* 输入框聚焦时, 相应的 label 字体颜色变黑 */
+    section:focus-within span {
+        color: #000;
+    }
 
+    .login-button {
+        font-weight: bold;
+        padding: 1.25rem;
+        font-size: 1rem;
+    }
 </style>
