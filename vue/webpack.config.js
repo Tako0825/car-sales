f20560bcd42cf7df76a4cbfaf1378ce8330c6caa
Tako <2060364922@qq.com>
2023-11-21 10:48:14 +0800

    使用MiniCssExtractPlugin优化css绘制性能、解决ico图标加载问题

--- vue/webpack.config.js
+++ vue/webpack.config.js
@@ -1,4 +1,5 @@
 const { resolve } = require("path") 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = webpackConfig = {
     mode: 'development',
@@ -6,5 +7,26 @@ module.exports = webpackConfig = {
         alias: {
             "@": resolve(__dirname,"src/")
         }
-    }
+    },
+    // 使用 MiniCssExtractPlugin 插件来提取 CSS 文件
+    module: {
+        rules: [
+            {
+            test: /\.css$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                // ... 其他 loader
+            ],
+            include: [resolve(__dirname, "src/")], // 根据你的项目结构调整
+            exclude: /node_modules/,
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'css/chunk-vendors.[contenthash].css', // 使用 contenthash 提高缓存命中率
+        }),
+        // 其他插件
+    ],
 }
